44bce4ef867878381de3c2ece7b471c9b1aee5af
Ahmet Can Ogreten <ahmetcan.ogreten@kuartis.com>
2022-07-30 23:31:12 +0300

    Tailwindcss added to project

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,8 @@
-
-
-<h1>
-    <img src="/to-space.jpg" alt="A man looking through the space" width=80%>
-    <p>Ahmet Can Ogreten</p>
-</h1>
-<h3>Just another Tech Enthusiast</h3>
+<img
+	class="m-auto mt-8 rounded-3xl"
+	src="/to-space.jpg"
+	alt="A man looking through the space"
+	width="80%"
+/>
+<h1 class="text-5xl text-center">Ahmet Can Ogreten</h1>
+<h3 class="text-center">Just another Tech Enthusiast</h3>
