49289792ad26c0bd79487e1c378806f046f3ea29
Ahmet Can Ogreten <ahmetcan.ogreten@kuartis.com>
2022-08-01 20:44:52 +0300

    app.js added for node-adapter to use on production

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,10 +10,9 @@ const config = {
 	})],
 
 	kit: {
-		adapter: adapter(),
-		prerender: {
-			default: true,
-		}
+		adapter: adapter({
+			out: 'build'
+		}),
 	}
 };
 
